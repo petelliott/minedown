b99b3363b627a1844e22af84056f2060dcceba00
Peter Elliott <pelliott@ualberta.ca>
2022-11-14 23:06:47 -0700

    use ascii.png to get glyph widths instead

--- glyph_gen.py
+++ glyph_gen.py
@@ -1,10 +1,21 @@
 import sys
+import PIL.Image
 
 print('const glyph_sizes = new Uint8Array([')
 
-with open(sys.argv[1], mode='rb') as f:
-    data = f.read()
-    for byte in data:
-        print(f"{byte}, ", end="")
+im = PIL.Image.open(sys.argv[1])
 
-print('])')
+for codepoint in range(256):
+    x = 8*(codepoint % 16)
+    y = 8*(codepoint // 16)
+    w = -1
+    for i in range(x, x+8):
+        for j in range(y, y+8):
+            if im.getpixel((i, j))[3] != 0:
+                w = max(w, i - x + 1)
+
+    if w == -1:
+        w = 4 # TODO: figure this out
+    print(f"{w+1}, ", end="")
+
+print(']);')
